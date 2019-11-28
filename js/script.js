7699072a4fd1c819829decb4cb6b7c3bc009a7be
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2019-11-28 14:40:10 +0300

    it will add the js css and html and it will create a calculator and style it

---
+++ js/script.js
@@ -0,0 +1,22 @@
+ var multiply = function(number1, number2) {
+   return number1 * number2;
+ };
+ var number1 = parseInt(prompt("Enter a number:"));
+ var number2 = parseInt(prompt("Enter another number:"));
+ alert(multiply(number1,number2));
+ 
+var divide=function(weight,height){
+    return weight/height;
+  }
+  var weight = parseInt(prompt("Weight in kg"));
+  var height = parseInt(prompt("Height in Cm"));
+  alert(divide(weight,height));
+
+  var multiply=function(fahrenheit,celcius){
+    return fahrenheit*celcius;
+  }
+  var fahrenheit = parseInt(prompt("Fahrenheit"));
+  var celcius = parseInt(prompt("Celcius"));
+  alert(multiply(fahrenheit,celcius));
+
+  
